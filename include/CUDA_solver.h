1f4e255b78a449086cba31b12654f0befa3c697b
Grzegorz Gurgula kbhit@agh.edu.pl (Paszynski) <gurgula@merlin.icsr.agh.edu.pl>
2016-09-04 12:28:53 +0100

    To fix

--- include/CUDA_solver.h
+++ include/CUDA_solver.h
@@ -18,7 +18,7 @@ class CUDA_solver : public bspline_fem_solver<T> {
 
     CUDA_solver(unsigned n) : _N(n)
     {
-        CUDA_prepare_device<T>(degree, _N);
+        CUDA_prepare_device(degree, _N);
     }
 
     virtual
@@ -35,43 +35,43 @@ class CUDA_solver : public bspline_fem_solver<T> {
          T,
          T)
     {
-        CUDA_init_fronts<T>(degree, _N);
+        CUDA_init_fronts(degree, _N);
     }
 
     virtual void
     eliminate()
     {
-        CUDA_eliminate<T>(degree, _N);
+        CUDA_eliminate(degree, _N);
     }
 
     virtual void
     solve_last_equation()
     {
-        CUDA_solve_last_equation<T>(degree, _N);
+        CUDA_solve_last_equation(degree, _N);
     }
     
     virtual void
     backward_substitution()
     {
-        CUDA_backward_substitution<T>(degree, _N);
+        CUDA_backward_substitution(degree, _N);
     }
     
     virtual T
     error(param_fun)
     {
-        return CUDA_error<T>(degree, _N);
+        return CUDA_error(degree, _N);
     }
 
     virtual void
     print_result(std::ostream &ostr)
     {
-        CUDA_print_result<T>(degree, _N, ostr);
+        CUDA_print_result(degree, _N, ostr);
     }
     
     virtual void
     debug()
     {
-        CUDA_debug<T>(degree, _N);
+        CUDA_debug(degree, _N);
     }
 };
 
